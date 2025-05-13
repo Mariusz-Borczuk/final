cb5a5b4723b36d92e1b8a1ab172cf76810d01c0a
Mariusz Borczuk <maniubrr@gmail.com>
2025-05-13 19:31:45 +0200

    remove unused components and styles; add index.css for scrollbar styling

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import MainLayout from "./components/ui/layouts/MainLayout.tsx";
+import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
